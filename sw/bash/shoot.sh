e1489620b5890b3ae71baf0c93e47b91d027c74b
daniw <dani.winz@bluewin.ch>
2015-06-02 00:37:14 +0200

    script improved with optional parameters for direction to shoot and device to write to

--- sw/bash/shoot.sh
+++ sw/bash/shoot.sh
@@ -1,11 +1,38 @@
-echo "l6480 goto 0" > /dev/rfcomm0; 
-echo "BLDC on" > /dev/rfcomm0; 
-sleep 16; 
-echo "l6480 goto 5500" > /dev/rfcomm0; 
-sleep 0.3; 
-echo "shoot" > /dev/rfcomm0; 
-sleep 2.5; 
-echo "BLDC off" > /dev/rfcomm0; 
-echo "l6480 goto 0" > /dev/rfcomm0;
-sleep 1; 
-echo "BLDC sound" > /dev/rfcomm0
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    position=0
+    dev=/dev/rfcomm0
+elif [ $# -eq 1 ]; then
+    position=$1
+    dev=/dev/rfcomm0
+elif [ $# -ge 2 ]; then
+    position=$1
+    dev=$2
+fi
+
+#initialize device, start bldc and wait until ready
+echo initialize device ...
+echo "l6480 goto 0" > $dev
+echo "BLDC on" > $dev
+sleep 15
+echo ready
+sleep 1
+
+# positioning
+echo aiming ...
+echo "l6480 goto $position" > $dev
+sleep 0.3
+
+#shooting
+echo shooting ...
+echo "shoot" > $dev
+sleep 2.5
+
+#prepare for reload and next round
+echo finished
+echo "BLDC off" > $dev
+echo "l6480 goto 0" > $dev
+sleep 1
+
+echo "BLDC sound" > $dev
+echo reload ...
